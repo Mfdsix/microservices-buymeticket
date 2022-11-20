a9e5e6ece16c6edadd5394b6d164b49c4da209e6
Mahfudz Ainur Rif'an <mfdsix.1nd0@gmail.com>
2022-11-21 01:40:00 +0700

    auth service ready to launch

---
+++ auth-service/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import { json } from 'body-parser'
+
+const port = 3000
+
+const app = express()
+app.use(json())
+
+app.listen(port, () => {
+    console.log(`Running Auth Service on :${port}`)
+})
