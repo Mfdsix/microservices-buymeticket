5d08fab86879cf0d662f1ba09aebfa4a3e7262b9
Mahfudz Ainur Rif'an <mfdsix.1nd0@gmail.com>
2022-11-30 16:19:07 +0700

    add user validation

--- auth-service/src/routes/signup.ts
+++ auth-service/src/routes/signup.ts
@@ -1,9 +1,29 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator'
 
 const router = express.Router()
 
-router.post("/api/users/sign-up", (req, res) => {
-    res.send("Hi me")
+router.post("/api/users/sign-up", [
+    body('email')
+    .isEmail()
+    .withMessage('Invalid Email'),
+    body('password')
+    .trim()
+    .isLength({
+        min: 4,
+        max: 20
+    })
+    .withMessage('Invalid Password')
+], (req: Request, res: Response) => {
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        return res.status(422).send(errors.array())
+    }
+
+    const { email, password } = req.body
+    console.log("creating user")
+    res.send({})
 })
 
 export {
